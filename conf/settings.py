d26493ac655caf9ffbde07a4e9e85300ba016c89
ilia <il.vsl0110@gmail.com>
2023-03-14 23:52:15 +0300

    add all

--- conf/settings.py
+++ conf/settings.py
@@ -43,7 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'application'
+    'application',
+    'booking'
 ]
 
 CRISPY_TEMPLATE_PACK = "bootstrap4"
